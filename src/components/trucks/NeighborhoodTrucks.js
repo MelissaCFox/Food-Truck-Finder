2a490afbebd35fd704b25bbccc269786573666b4
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-12-14 10:45:35 -0600

    date picker now sets the correct date for the component to appropriately filter truck locations

--- src/components/trucks/NeighborhoodTrucks.js
+++ src/components/trucks/NeighborhoodTrucks.js
@@ -5,63 +5,62 @@ import { TruckList } from "./TruckList"
 
 
 export const NeighborhoodTruckList = () => {
-const [neighborhoods, setNeighborhoods] = useState([])
-const [dateString, setDateString] = useState("")
-const history = useHistory()
-const [dateForList, setDateForList] = useState({})
+    const [neighborhoods, setNeighborhoods] = useState([])
+    const [dateString, setDateString] = useState("")
+    const history = useHistory()
+    const [dateForList, setDateForList] = useState("")
 
-useEffect(() => {
-    let newDate = new Date()
-    setDateForList(newDate)
-},[])
-
-
-
-useEffect(() => {
-    const date = new Date(dateForList)
-
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    const day = date?.getDay()
-    const number = date?.getDate()
-    const month = date?.getMonth()
-    const year = date?.getFullYear()
-    
-    setDateString(`${days[day]} ${months[month]} ${number}, ${year}`)
-    console.log(date)
-
-},[dateForList])
+    useEffect(() => {
+        let newDate = new Date()
+        setDateForList(newDate)
+    }, [])
 
 
-useEffect(() => {
-    NeighborhoodRepository.getAll().then(setNeighborhoods)
-},[])
+
+    useEffect(() => {
+        let date = new Date(dateForList)
+
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        const day = date?.getDay()
+        const number = date?.getDate()
+        const month = date?.getMonth()
+        const year = date?.getFullYear()
+
+        setDateString(`${days[day]} ${months[month]} ${number}, ${year}`)
+
+    }, [dateForList])
 
 
-return (
-    <>
-    <div className="date">
-        <input type="date" onChange={(event) => {
-            const newDate = event.target.value
-            const newDateString = new Date(newDate)
-            console.log(newDateString)
-            setDateForList(newDateString)
-        }}></input>
-        <div className="date-string">{dateString}</div>
-    </div>
-    <div className="filter-options"></div>
-    <ul className="neighborhoods">
-    {
-        neighborhoods.map(neighborhood => {
-            return <li className="card neighborhood" key={neighborhood.id}>
-                <button onClick={() => { history.push(`/neighborhoods/${neighborhood?.id}`) }}>{neighborhood.name}</button>
-                <TruckList key={`neighborhood--${neighborhood.id}`} neighborhood={neighborhood} date={dateForList}/>
+    useEffect(() => {
+        NeighborhoodRepository.getAll().then(setNeighborhoods)
+    }, [])
 
-            </li>
-        })
-    }
-    </ul>
-    </>
-)
+
+    return (
+        <>
+            <div className="date">
+                <input type="date" onChange={(event) => {
+                    const newDate = event.target.value
+                    const parsedDate = Date.parse(newDate) + 86400000
+                    const accurateDate = new Date(parsedDate)
+                    setDateForList(accurateDate)  
+                }}></input>
+                <div className="date-string">{dateString}</div>
+            </div>
+            <div className="filter-options"></div>
+            <ul className="neighborhoods">
+                {
+                    neighborhoods.map(neighborhood => {
+                        return <li className="card neighborhood" key={neighborhood.id}>
+                            <button onClick={() => { history.push(`/neighborhoods/${neighborhood?.id}`) }}>{neighborhood.name}</button>
+                            <TruckList key={`neighborhood--${neighborhood.id}`} neighborhood={neighborhood} date={dateForList} />
+
+                        </li>
+                    })
+                }
+            </ul>
+        </>
+    )
 
 }
