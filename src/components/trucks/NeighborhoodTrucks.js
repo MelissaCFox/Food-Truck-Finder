1c0ec493720a26b835fa153fad698818f5b035c4
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-12-13 23:22:26 -0600

    date picker triggers a new list of trucks to render but the date it's capturing is several hours off from local time, which results in a difference of an entire day at some points

--- src/components/trucks/NeighborhoodTrucks.js
+++ src/components/trucks/NeighborhoodTrucks.js
@@ -8,10 +8,18 @@ export const NeighborhoodTruckList = () => {
 const [neighborhoods, setNeighborhoods] = useState([])
 const [dateString, setDateString] = useState("")
 const history = useHistory()
-
-let date = new Date()
+const [dateForList, setDateForList] = useState({})
 
 useEffect(() => {
+    let newDate = new Date()
+    setDateForList(newDate)
+},[])
+
+
+
+useEffect(() => {
+    const date = new Date(dateForList)
+
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const day = date?.getDay()
@@ -20,18 +28,25 @@ useEffect(() => {
     const year = date?.getFullYear()
     
     setDateString(`${days[day]} ${months[month]} ${number}, ${year}`)
+    console.log(date)
 
-},[date])
+},[dateForList])
 
 
 useEffect(() => {
     NeighborhoodRepository.getAll().then(setNeighborhoods)
-},[dateString])
+},[])
 
 
 return (
     <>
     <div className="date">
+        <input type="date" onChange={(event) => {
+            const newDate = event.target.value
+            const newDateString = new Date(newDate)
+            console.log(newDateString)
+            setDateForList(newDateString)
+        }}></input>
         <div className="date-string">{dateString}</div>
     </div>
     <div className="filter-options"></div>
@@ -40,7 +55,7 @@ return (
         neighborhoods.map(neighborhood => {
             return <li className="card neighborhood" key={neighborhood.id}>
                 <button onClick={() => { history.push(`/neighborhoods/${neighborhood?.id}`) }}>{neighborhood.name}</button>
-                <TruckList key={`neighborhood--${neighborhood.id}`} neighborhood={neighborhood} date={date}/>
+                <TruckList key={`neighborhood--${neighborhood.id}`} neighborhood={neighborhood} date={dateForList}/>
 
             </li>
         })
